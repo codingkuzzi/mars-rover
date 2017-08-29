f046999de6c04333fd78544d0b2c9c5a1d37b19b
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-08-29 16:54:48 -0700

    add another API key call and carousel functionality for displaying photos

--- src/app/mars-rover-api-photos.service.ts
+++ src/app/mars-rover-api-photos.service.ts
@@ -12,7 +12,7 @@ export class MarsRoverApiPhotosService {
 
 
   getByDateAndCamera(date: string, camera: string) {
-    return this.http.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" +date+ "&camera=" +camera+ "&api_key="+marsRoverKey)
+    return this.http.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-01-01&camera=mast&api_key=YFcL2M7cMBQ8j1VRQTyCO1ejj1SDqgY3k2FJWdxc")
   }
   saveImages(date: string, camera: string) {
     return this.http.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + date + "&camera=" + camera + "&api_key=" + marsRoverKey)
